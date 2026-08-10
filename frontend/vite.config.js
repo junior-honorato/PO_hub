0098a230bb1e02d7470a50a63f5cce8cfe03eff6
junior-honorato <arlindo.junior@sicoob.com.br>
2026-08-09 21:52:37 -0300

    fix: render Cloud Database Status view when accessing Database tab in Cloud environment to prevent UI crash

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,16 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    outDir: '../backend/static',
+    emptyOutDir: true,
+  },
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        secure: false,
-      },
-      '/demands': {
-        target: 'http://localhost:5000',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false,
       }
